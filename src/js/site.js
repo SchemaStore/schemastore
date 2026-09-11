34f5391816dfd03bdcef9cb058d89027f5b1a42c
Sanjaiyan Parthipan <parthipankalayini@gmail.com>
2026-09-12 02:44:25 +0530

    Fix search filtering by using textContent for case-insensitive comparison

--- src/js/site.js
+++ src/js/site.js
@@ -65,7 +65,7 @@
 
         for (const li of ul.childNodes) {
           li.style.display =
-            li.textContent.trim().toLowerCase().indexOf(value) > -1 ? 'block' : 'none'
+            li.textContent.toLowerCase().indexOf(value) > -1 ? 'block' : 'none'
         }
       }, 300)
     },
